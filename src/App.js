a6102d5e5c718f77f737290b8c5b8f3cffe8dda0
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-11-24 10:41:48 +0530

    interview test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Table from './table/Table';
 
 function App() {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products/').then((res) => {
+      console.log(res.data);
+      setApiData([...res.data]);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {apiData.map((item) => {
+        return (
+          <div key={item.id}>
+            {' '}
+            <Table title={item.title} />{' '}
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/table/Table.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Table = ({ title }) => {
+  return (
+    <div>
+      <ul>
+        <li>{title}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Table;
